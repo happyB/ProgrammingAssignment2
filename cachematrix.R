269d6c52627e9d9f5fe21eaac587768d65a02102
happyB <bfoth@gmx.net>
2014-04-26 18:05:53 +0100

    The frist change in this file, adding 2 letters

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,5 @@
 ## Put comments here that give an overall description of what your
-## functions do
+## functions do BF
 
 ## Write a short comment describing this function
 # A function that can create a 'matrix' object that is capable of caching its own inverse
